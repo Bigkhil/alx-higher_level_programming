e880ca6e1036e2d077b8e3147bc73466f9646fa8
Mohamed khalil <khm40005000@outlook.com>
2024-03-14 02:26:01 +0200

    some changes

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * check_cycle - checks for a cycle
+ * @list: pointer to a node in list'
+ * Return: 1 or 0
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *temp = list;
+
+	while (list != NULL)
+	{
+		list = list->next;
+		if (list == temp)
+			return (1);
+	}
+	return (0);
+}
