97c998045435286ab46dcd57e06125719f3f9896
Mohamed khalil <khm40005000@outlook.com>
2024-03-19 01:31:56 +0200

    some changes

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     new_list = []
-    new_list = my_list[:(idx + 1)] + my_list[(idx + 1):]
+    new_list = my_list[:idx] + my_list[(idx + 1):]
     return new_list
