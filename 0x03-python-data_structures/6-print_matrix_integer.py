2234492743c7d57500dec4d4baac44e888a5ccaf
Mohamed khalil <khm40005000@outlook.com>
2024-03-19 00:19:56 +0200

    some changes

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix:
         for i in matrix:
+            if len(matrix) == 1 and len(matrix[0]) == 0:
+                print("")
+                continue
             for j in i:
                 print("{:d}".format(j), end=" " if j != i[-1] else "\n")
