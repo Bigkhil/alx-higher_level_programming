8d4f34a5404917aefc7190ea3cc4ffbf35e387ed
Mohamed khalil <khm40005000@outlook.com>
2024-03-21 03:14:48 +0200

    some changes

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if bool(a_dictionary):
-        max = 0
-        if a_dictionary[a_dictionary.keys()[0]] is not None:
-            max = a_dictionary[a_dictionary.keys()[0]]
-        ans = a_dictionary.keys()[0]
+        max_value = max(a_dictionary.values())
         for i in a_dictionary.keys():
-            if a_dictionary[i] is not None and a_dictionary[i] >= max:
-                max = a_dictionary[i]
-                ans = i
-        return ans
+            if a_dictionary[i] == max:
+                return i
     return None
