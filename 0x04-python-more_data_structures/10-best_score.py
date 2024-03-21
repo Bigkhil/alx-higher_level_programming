55e60d16d5d76b9bfe6c53c1dc865fbe89c4e02b
Mohamed khalil <khm40005000@outlook.com>
2024-03-21 02:59:45 +0200

    some changes

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if bool(a_dictionary):
+        max = a_dictionary[a_dictionary.keys()[0]]
+        for i in a_dictionary.keys():
+            if a_dictionary[i] > max:
+                max = a_dictionary[i]
+        return max
+    return None
