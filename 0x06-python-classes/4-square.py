d91cd38ebb6c3014359b380b391c05c9b17c59c4
Mohamed khalil <khm40005000@outlook.com>
2024-04-12 17:04:07 +0200

    some changes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -23,4 +23,9 @@ class Square:
     @size.setter
     def size(self, value):
         """this is setter"""
-        self.__size = value
+        if type(value) is not int:
+             raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
