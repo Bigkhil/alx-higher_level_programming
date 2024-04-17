f8b9ad29245f8dead286aeff43a3eec2258d0ab7
Mohamed khalil <khm40005000@outlook.com>
2024-04-17 20:33:23 +0200

    some changes

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""json file deserialization"""
+import json
+
+
+def load_from_json_file(filename):
+    """json file function"""
+    with open(filename, 'r') as file:
+        return json.loads(filename)
