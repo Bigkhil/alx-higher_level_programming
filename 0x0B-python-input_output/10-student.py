924c95ed25da31f7584eebc8e9beee4850e14588
Mohamed khalil <khm40005000@outlook.com>
2024-04-18 15:11:37 +0200

    some changes

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Student class module"""
+
+
+class Student:
+    """Student class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
+            d = self.__dict__
+            ret = {}
+            for x in attrs:
+                ret[x] = d[x]
+            return ret
+        return self.__dict__
