87240d58e440b593b417dd64c155e9eb27413063
Mohamed khalil <khm40005000@outlook.com>
2024-04-22 14:15:02 +0200

    updated square class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -83,4 +83,29 @@ class Rectangle(Base):
         print(((" " * self.__x) + ("#" * self.__width) + '\n') * self.__height, end="")
 
     def __str__(self):
-        return f"({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+        return f"([Rectangle] {self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+
+    def update(self, *args, **kwargs):
+        if len(args) != 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.__width = args[1]
+            if len(args) >= 3:
+                self.__height = args[2]
+            if len(args) >= 4:
+                self.__x = args[3]
+            if len(args) >= 5:
+                self.__y = args[4]
+        else:
+            if len(kwargs) != 0:
+                if 'width' in kwargs:
+                    self.__width = kwargs['width']
+                if 'height' in kwargs:
+                    self.__height = kwargs['height']
+                if 'id' in kwargs:
+                    self.id = kwargs['id']
+                if 'x' in kwargs:
+                    self.__x = kwargs['x']
+                if 'y' in kwargs:
+                    self.__y = kwargs['y']
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''Square module'''
+from .rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+    
+    def __str__(self):
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.height}"
+
+    @property
+    def size(self):
+        return self.height
+    
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        
