18204a894af985409f5d1bf32f776455c39ed16a
Mohamed khalil <khm40005000@outlook.com>
2024-04-25 10:39:37 +0200

    some changes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,7 +19,6 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-    @staticmethod
     def to_json_string(list_dictionaries):
         """converts the list of dictionaries to json string
         Args:
