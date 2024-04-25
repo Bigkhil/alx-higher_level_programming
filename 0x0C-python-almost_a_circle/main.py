d789b6c93413139123cc5912cb0f54cad66df375
Mohamed khalil <khm40005000@outlook.com>
2024-04-25 22:38:36 +0200

    updated square class

--- 0x0C-python-almost_a_circle/main.py
+++ 0x0C-python-almost_a_circle/main.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-""" 15-main """
+""" 5-main """
 from models.rectangle import Rectangle
 
 if __name__ == "__main__":
 
-    r1 = Rectangle(10, 7, 2, 8)
-    r2 = Rectangle(2, 4)
-    Rectangle.save_to_file([r1, r2])
+    r1 = Rectangle(4, 6, 2, 1, 12)
+    print(r1)
 
-    with open("Rectangle.json", "r") as file:
-        print(file.read())
+    r2 = Rectangle(5, 5, 1)
+    print(r2)
