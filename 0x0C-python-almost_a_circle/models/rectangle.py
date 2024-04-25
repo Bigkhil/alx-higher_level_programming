86dada7e2dc13719786efc9fd6a993c8c725061f
Mohamed khalil <khm40005000@outlook.com>
2024-04-25 22:32:14 +0200

    some changes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -105,8 +105,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """str method"""
-        return f"[Rectangle]({self.id}) {self.__x}/{self.__y}
-        - {self.__width}/{self.__height}"
+        return f"[Rectangle]({self.id}) {self.__x}/{self.__y} - \
+        {self.__width}/{self.__height}"
 
     def update(self, *args, **kwargs):
         """update method"""
