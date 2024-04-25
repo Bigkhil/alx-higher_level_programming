9e5d3f3b56c242d7165a61580d3b3afe42452484
Mohamed khalil <khm40005000@outlook.com>
2024-04-25 10:32:43 +0200

    some changes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,11 +4,13 @@ import json
 from .rectangle import Rectangle
 from .square import Square
 
+
 class Base:
     """class Base doc"""
     __nb_objects = 0
+
     def __init__(self, id=None):
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
@@ -16,15 +18,15 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """converts the list of dictionaries into json string"""
-        if list_dictionaries == None:
+        """json string"""
+        if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
         """saves the json string to file"""
-        if list_objs != None:
+        if list_objs is not None:
             if isinstance(list_objs[0], Rectangle):
                 with open('Rectangle.json', 'w') as file:
                     for i in list_objs:
