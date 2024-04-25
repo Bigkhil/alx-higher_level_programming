6b82a48efdf79d2c7c8e48942af81cde61e5da7c
Mohamed khalil <khm40005000@outlook.com>
2024-04-25 15:09:01 +0200

    some changes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""class base"""
+"""Base class"""
+import os.path
 import json
-from .rectangle import Rectangle
-from .square import Square
 
 
 class Base:
-    """class Base doc"""
+    """Base class"""
 
     __nb_objects = 0
 
@@ -18,29 +17,62 @@ class Base:
             self.id = id
         else:
             Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+            self.id = self.__nb_objects
 
-    @staticmethod
     def to_json_string(list_dictionaries):
-        """converts the list of dictionaries to json string
-        Args:
-            list_dictionaries: list of dictionaries
-        """
-        if list_dictionaries is None:
-            return "[]"
-        return json.dumps(list_dictionaries)
+        """to json string"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return ("[]")
+        return (json.dumps(list_dictionaries))
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """saves the json string to file"""
+        """save to file"""
+        filename = cls.__name__ + ".json"
+        list = []
         if list_objs is not None:
-            if isinstance(list_objs[0], Rectangle):
-                with open('Rectangle.json', 'w') as file:
-                    for i in list_objs:
-                        file.write(cls.to_json_string(i))
-            else:
-                with open('Square.json', 'w') as file:
-                    for i in list_objs:
-                        file.write(cls.to_json_string(i))
-        else:
-            pass
+            for obj in list_objs:
+                list.append(obj.to_dictionary())
+        with open(filename, "w") as f:
+            f.write(cls.to_json_string(list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON"""
+        import json
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """create method"""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        if cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return (dummy)
+
+    @classmethod
+    def load_from_file(cls):
+        """Load from file"""
+        filename = cls.__name__ + ".json"
+        new_list = []
+        if os.path.exists(filename):
+            with open(filename, "r") as f:
+                new_list = cls.from_json_string(f.read())
+            for dict in new_list:
+                new_list.append(cls.create(**dict))
+        return new_list
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """save to file csv"""
+        filename = cls.__name__ + ".csv"
+        list = []
+        if list_objs is not None:
+            for obj in list_objs:
+                list.append(obj.to_dictionary())
+        with open(filename, "w") as f:
+            f.write(cls.to_json_string(list))
