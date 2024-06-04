845d11d4b17d97326da3db55e018be07bd923f1b
Mohamed khalil <khm40005000@outlook.com>
2024-06-04 12:19:15 +0300

    some changes

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- create database and user
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 CREATE user if not exists 'user_0d_2'@'localhost' identified by 'user_0d_2_pwd';
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 GRANT select on hbtn_0d_2.* to 'user_0d_2'@'localhost';
 flush privileges;
