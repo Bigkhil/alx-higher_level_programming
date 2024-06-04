af2e40fbc19a3e7e4fee5004e57d810f366bf178
Mohamed khalil <khm40005000@outlook.com>
2024-06-04 11:53:48 +0300

    some changes

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- create database and user
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 CREATE user if not exists 'user_0d_2'@'localhost' identified by 'user_0d_2_pwd';
-GRANT select on 'hbtn_0d_2'.* to 'user_0d_2'@'localhost';
+GRANT select on hbtn_0d_2.* to 'user_0d_2'@'localhost';
