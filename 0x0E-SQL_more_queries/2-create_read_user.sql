6684968351f76d3afef8e476e416ce4645408eaf
Mohamed khalil <khm40005000@outlook.com>
2024-06-04 12:06:00 +0300

    some changes

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,3 +2,4 @@
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 CREATE user if not exists 'user_0d_2'@'localhost' identified by 'user_0d_2_pwd';
 GRANT select on `hbtn_0d_2`.* to 'user_0d_2'@'localhost';
+flush privileges;
--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,6 @@
 -- create a table in a database
 CREATE TABLE IF NOT EXISTS `force_name`
 (
-	`id` INT primary key,
+	`id` INT,
 	`name` VARCHAR(256) NOT NULL
 );
