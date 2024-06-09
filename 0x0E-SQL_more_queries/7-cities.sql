ca8f9c07dcd2f8a4e7ffc46ba0f52868109740b6
Mohamed khalil <khm40005000@outlook.com>
2024-06-09 20:12:40 +0300

    some changes

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,6 @@
 -- create new database and table
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS `cities`
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities`
 (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	state_id INT NOT NULL,
