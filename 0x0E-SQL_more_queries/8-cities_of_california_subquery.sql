d80eefaa57ea28539da031f02ea1d22838584ae0
Mohamed khalil <khm40005000@outlook.com>
2024-06-09 20:47:35 +0300

    some changes

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- list all cities of california that are found in hbtn_0d_usa
-SELECT c.`id`, c.`name` FROM `hbtn_0d_usa`.`cities` AS c, `hbtn_0d_usa`.`states` AS s
+SELECT c.`id`, c.`name` FROM `cities` AS c, `states` AS s
 WHERE s.`name` = 'California' AND
 s.`id` = c.`state_id`;
