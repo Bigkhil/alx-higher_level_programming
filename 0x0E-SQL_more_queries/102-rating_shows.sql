d5f9d9e0fd3e910059e3552b177ca3a1d6e5629f
Mohamed khalil <khm40005000@outlook.com>
2024-06-12 16:42:32 +0300

    some changes

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- list all shows by their ratings
-SELECT tv_shows.title, sum(tv_show_ratings.rate) AS rating
+SELECT tv_shows.title AS title, sum(tv_show_ratings.rate) AS rating
 FROM tv_show_ratings
 JOIN tv_shows ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.title
-ORDER BY tv_show_ratings.rate DESC;
+ORDER BY rating DESC;
