c391152e5bfc196270232c6d4dd0e1dc7796ade3
Mohamed khalil <khm40005000@outlook.com>
2024-06-12 16:30:57 +0300

    some changes

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,6 @@
+-- list all shows by their ratings
+SELECT tv_shows.title, sum(tv_show_ratings.rate)
+FROM tv_show_ratings
+JOIN tv_shows ON tv_shows.id = tv_show_ratings.id
+GROUP BY tv_shows.title
+ORDER BY tv_show_ratings.rate DESC;
