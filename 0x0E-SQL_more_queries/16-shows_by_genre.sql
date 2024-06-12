f97b92aa0572c305e280becb4a159adb3fe77c72
Mohamed khalil <khm40005000@outlook.com>
2024-06-12 15:59:22 +0300

    some changes

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- list all names of all shows with their related genres
+SELECT tv_shows.title AS title, tv_genres.name AS `name`
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY title, `name`;
