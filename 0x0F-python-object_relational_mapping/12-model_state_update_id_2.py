67476c7e45b162d16365dcd48090927845b12aa8
Mohamed khalil <khm40005000@outlook.com>
2024-06-30 00:51:50 +0300

    some changes

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''this script should list all states in the database using sqlalchemy'''
+import MySQLdb
+import sys
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+def main():
+    '''main fucntion to execute when using the script'''
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]
+                                   ), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    row = session.query(State).filter(State.id == 2).first()
+    if row:
+        row.name = "New Mexico"
+        session.commit()
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
