4da056d9b07113a1f40d63e58226a8392ba2bb73
Mohamed khalil <khm40005000@outlook.com>
2024-06-29 17:23:44 +0300

    some changes

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''this script should list all states in the database that start with N'''
+import MySQLdb
+import sys
+
+
+def main():
+    '''main fucntion to execute when using the script'''
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+    cursor = conn.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name\
+                   FROM cities INNER JOIN states")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
