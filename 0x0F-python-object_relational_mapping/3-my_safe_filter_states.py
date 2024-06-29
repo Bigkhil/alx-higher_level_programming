b34ed784c1496693a7721b6f4677f905a7714475
Mohamed khalil <khm40005000@outlook.com>
2024-06-29 17:12:34 +0300

    some changes

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''this script should list all states in the database that start with N'''
+import MySQLdb
+import sys
+
+
+def main():
+    '''main fucntion to execute when using the script'''
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+    cursor = conn.cursor()
+    param = '%' + sys.arg[4] + '%'
+    cursor.execute("SELECT * from states WHERE `name` LIKE BINARY %s;", (param, ))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
