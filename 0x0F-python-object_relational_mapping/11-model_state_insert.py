8573f8798d16fc67abd6886c22f95e4c101deb94
Mohamed khalil <khm40005000@outlook.com>
2024-06-30 00:26:19 +0300

    some changes

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''this script should list all states in the database using sqlalchemy'''
+import MySQLdb
+import sys
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+def main():
+    '''main fucntion to execute when using the script'''
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]
+                                   ), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    rows = session.query(State).filter(State.name == sys.argv[4]).first()
+    if rows:
+        print(rows.id)
+    else:
+        print("Not found")
+
+
+if __name__ == "__main__":
+    main()
