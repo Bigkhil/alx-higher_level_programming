1104e68f520c7342a5290170af31fccc6f5a0044
Mohamed khalil <khm40005000@outlook.com>
2024-07-05 02:18:31 +0300

    some changes

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/python3
+#!/bin/bash
 # this script takes in a URL, sends a request to that URL, and displays the size of the body of the response
 curl -s -o /dev/null -w '%{size_download}' $1
