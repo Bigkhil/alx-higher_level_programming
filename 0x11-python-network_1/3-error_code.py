a8d826eba6b978198ad34d6b40f9cc595370cf18
Mohamed khalil <khm40005000@outlook.com>
2024-07-05 21:45:19 +0300

    some changes

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-'''
-takes in a URL and an email, sends a POST request to the passed URL with the
-email as a parameter, and displays the body of the response
-'''
-import urllib.parse
-import urllib.request
-import sys
+"""initializate"""
+from sys import argv
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
 
 
-def main():
-    '''this is the main function'''
-    url = sys.argv[1]
-    email = {'email': sys.argv[2]}
-    data = urllib.parse.urlencode(email).encode('utf-8')
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as response:
-        body = response.read()
-        print(body.decode('utf-8'))
+def post_email():
+    """ script that takes in a URL and an email, sends a POST
+    request to the passed URL with the email as a parameter,
+    and displays the body of the response (decoded in utf-8)"""
+    url = argv[1]
+    email = {'email': argv[2]}
+
+    data = urlencode(email).encode('utf-8')
+    req = Request(url, data)
+    with urlopen(req) as response:
+        print(response.read().decode('utf-8'))
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    post_email()
