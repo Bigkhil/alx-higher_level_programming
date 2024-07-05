73be4759948ae0cea703cc0b98efd610a1883a80
Mohamed khalil <khm40005000@outlook.com>
2024-07-05 16:15:27 +0300

    some changes

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 # this script gets the peak from an array
 def find_peak(list_of_integers):
-	'''this fucntion gets a peak from a list of integers'''
-	f = 0
-	l = len(list_of_integers) - 1
+    '''this fucntion gets a peak from a list of integers'''
+    first = 0
+    last = len(list_of_integers) - 1
 
-	if len(list_of_integers) == 0:
-		return None
-	while f <= l:
-		if list_of_integers[f] > list_of_integers[f + 1]:
-			return list_of_integers[f]
-		if list_of_integers[l] > list_of_integers[l - 1]:
-			return list_of_integers[l]
-		l = l - 1
-		f = f + 1
-	return list_of_integers[f]
+    if len(list_of_integers) == 0:
+        return None
+    while first <= last:
+        if list_of_integers[first] > list_of_integers[first + 1]:
+            return list_of_integers[first]
+        if list_of_integers[last] > list_of_integers[last - 1]:
+            return list_of_integers[last]
+        last = last - 1
+        first = first + 1
+    return list_of_integers[first]
