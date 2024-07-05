2a434dec75d5a3797eb4226e7256373990ed6c0e
Mohamed khalil <khm40005000@outlook.com>
2024-07-05 21:11:04 +0300

    some changes

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,7 +12,7 @@ def main():
     '''this is the main function'''
     url = sys.argv[1]
     email = sys.argv[2]
-    data = urllib.parse.urlencode({'email' : email}).encode('utf-8')
+    data = urllib.parse.urlencode({'email ': email}).encode('utf-8')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         body = response.read()
