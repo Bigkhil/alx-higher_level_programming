fa03bea6db08df6356117d32e6203b9f7f738e17
Mohamed khalil <khm40005000@outlook.com>
2024-07-06 17:46:09 +0300

    some changes

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''this script fetches a url and prints the response using requests module'''
+import requests
+import sys
+
+
+def main():
+    '''
+    this script fetches a url
+    and prints the response using requests module
+    '''
+    url = sys.argv[1]
+    email = {f"email: {sys.argv[2]}"}
+    response = requests.post(url, email)
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
