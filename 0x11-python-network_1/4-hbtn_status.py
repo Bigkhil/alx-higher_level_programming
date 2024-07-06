1ce78774182002df6871aad1d9de0a98bc3acb01
Mohamed khalil <khm40005000@outlook.com>
2024-07-06 17:31:21 +0300

    some changes

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,11 +4,15 @@ import requests
 
 
 def main():
-	url = "https://alx-intranet.hbtn.io/status"
-	response = requests.get(url)
-	print(f"type: {response}")
-	print(f"content: {response.text}")
+    '''
+    this script fetches a url
+    and prints the response using requests module
+    '''
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+    print(f"type: {response}")
+    print(f"content: {response.text}")
 
 
 if __name__ == "__main__":
-	main()	
+    main()
