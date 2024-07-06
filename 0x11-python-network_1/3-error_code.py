e7b0da0cc063929bc3756232bc46599570b3f9e7
Mohamed khalil <khm40005000@outlook.com>
2024-07-06 17:16:05 +0300

    some changes

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,20 +3,20 @@
 takes in a URL and an email, sends a POST request to the passed URL with the
 email as a parameter, and displays the body of the response
 '''
-import urllib.parse
 import urllib.request
+import urllib.error
 import sys
 
 
 def main():
     '''this is the main function'''
     url = sys.argv[1]
-    email = {'email': sys.argv[2]}
-    data = urllib.parse.urlencode(email).encode('utf-8')
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as response:
-        body = response.read()
-        print(body.decode('utf-8'))
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as error:
+        print("Error code: {}", error.code)
 
 
 if __name__ == "__main__":
