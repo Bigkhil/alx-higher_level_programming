febb878e50dfb23e5ab090b311f85990799a70f9
Mohamed khalil <khm40005000@outlook.com>
2024-07-27 00:06:34 +0300

    some changes

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 '''this script creates the City class'''
+
 from model_state import Base, State
 from sqlalchemy import VARCHAR, Column, Integer, String
 
+
 class City(Base):
     '''this is the cities table base class'''
+
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
